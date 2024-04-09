1921cceaa79d0d6f8836c8923a14d0af881da631
rabiebn <rabie.bnmr@gmail.com>
2024-04-09 22:47:33 +0000

    0: Initial commit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module has function number_of_subscribers"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Returns the number of subscribers for a given subreddit"""
+    base_url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+
+    response = requests.get(base_url,
+                            headers={"User-Agent": "whiteJhendrix"},
+                            allow_redirects=False)
+
+    if response.status_code == 200:
+        return response.json()["data"]["subscribers"]
+    
+    return 0
+
