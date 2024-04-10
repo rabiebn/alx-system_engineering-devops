88557863ee623619938ded019f255d7c45927896
rabiebn <rabie.bnmr@gmail.com>
2024-04-10 00:24:57 +0000

    1: handle server denial of service

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -3,12 +3,19 @@
 Module has function top_ten
 """
 import requests
+from time import sleep
 
 
 def top_ten(subreddit):
     """Print titles of the first 10 hot posts"""
     base_url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    response = requests.get(base_url, allow_redirects=False)
+    while True:
+        try:
+            response = requests.get(base_url, allow_redirects=False)
+            break
+        except requests.exceptions.ConnectionError as e:
+            sleep(5)
+            continue
 
     if response.status_code == 200:
         hot_posts = response.json()["data"]["children"]
