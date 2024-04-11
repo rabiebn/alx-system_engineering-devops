cc87db42de65fd60f73ef6d3870b4c03e6c47554
rabiebn <rabie.bnmr@gmail.com>
2024-04-11 22:46:50 +0000

    1: make sure to get 200 status

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -10,18 +10,17 @@ def top_ten(subreddit):
     """Print titles of the first 10 hot posts"""
     base_url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     while True:
-        try:
-            response = requests.get(base_url, allow_redirects=False)
+        response = requests.get(base_url, allow_redirects=False)
+        if response.status_code == 200:
             break
-        except requests.exceptions.ConnectionError as e:
+        else:
             sleep(5)
             continue
 
-    if response.status_code == 200:
-        hot_posts = response.json()["data"]["children"]
-        for i in range(0, 9):
-            try:
-                print(hot_posts[i]["data"]["title"])
-            except Exception as e:
-                return
+    hot_posts = response.json()["data"]["children"]
+    for i in range(10):
+        try:
+            print(hot_posts[i]["data"]["title"])
+        except Exception as e:
+            return
     return
