655fe78c27b14e1b4474056c46437f1ca966743f
rabiebn <rabie.bnmr@gmail.com>
2024-04-27 16:36:00 +0100

    .

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
 """Module has function number_of_subscribers"""
 import requests
-from time import sleep
 
 
 def number_of_subscribers(subreddit):
     """Returns the number of subscribers for a given subreddit"""
-    url = 'https://www.reddit.com'
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     header = {
         'Accept': 'application/json',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
     }
-    response = requests.get('{}/r/{}/about/.json'.format(url, subreddit),
-                            headers=header,
-                            allow_redirects=False
-                            )
+    response = requests.get(url, headers=header, allow_redirects=False)
     if response.status_code == 200:
-        return response.json()['data']['subscribers']
+        data = response.json().get("data")
+        return data.get("subscribers")
     return 0
