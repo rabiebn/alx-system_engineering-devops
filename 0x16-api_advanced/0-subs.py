de73285e5dea53ce3d758b66754d25486751e0a2
rabiebn <rabie.bnmr@gmail.com>
2024-04-27 16:43:04 +0100

    .

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,10 +6,7 @@ import requests
 def number_of_subscribers(subreddit):
     """Returns the number of subscribers for a given subreddit"""
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    header = {
-        'Accept': 'application/json',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
-    }
+    header = {'User-Agent': 'mr. Me'}
     response = requests.get(url, headers=header, allow_redirects=False)
     if response.status_code == 200:
         data = response.json().get("data")
