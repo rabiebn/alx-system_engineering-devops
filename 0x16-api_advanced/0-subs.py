7e9554c631a1408c33f7dd7675a2a5d0988f14f8
rabiebn <rabie.bnmr@gmail.com>
2024-04-27 16:51:08 +0100

    initial commit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,7 +8,7 @@ def number_of_subscribers(subreddit):
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     header = {'User-Agent': 'mr. Me'}
     response = requests.get(url, headers=header, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json().get("data")
-        return data.get("subscribers")
-    return 0
+    if response.status_code == 404:
+        return 0
+    data = response.json().get("data")
+    return data.get("subscribers")
